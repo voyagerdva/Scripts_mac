4014bba76c69bf01433e965563b8e5f2d968d0b3
Kirill Zhuravlev <48881889+voyagerdva@users.noreply.github.com>
2021-06-20 20:35:57 +0300

    begin promramm fingTC

--- run_mtr/run_mtr.sh
+++ run_mtr/run_mtr.sh
@@ -3,9 +3,11 @@
 rm mtr-report.txt 2>/dev/null
 file="testcenters.txt"
 IFS=$'\n'
+
 for host in $(cat $file)
 do
 echo "################ $host ##########################" >> mtr-report.txt
 mtr -r -m15 -c5 $host >> mtr-report.txt
+echo ""
 echo ""
 done
