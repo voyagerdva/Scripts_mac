07ee69bdc673b8b409e2232382b4d81da1386900
Kirill Zhuravlev <48881889+voyagerdva@users.noreply.github.com>
2021-07-29 01:35:53 +0300

    added search TC for nodes list

--- qqTC/createListNodes.py
+++ qqTC/createListNodes.py
@@ -3,27 +3,12 @@ import sys
 import re
 
 print("Enter the list of nodes you want to find TC for. Press CTRL+D for finish and apply:")
-textList = re.split(r'[\n,ovpn,hydra,ipsec]', sys.stdin.read())
-print(textList)
-# nameDir = "_".join(textList)
-# print("The name of new directory is: ", nameDir)
-#
-# pathFull = f"/Users/zhuk/SIBEDGE/PROJECT/{nameDir}"
-# os.mkdir(pathFull)
-#
-# file1 = f"{textList[0]}_NEW.yml"
-# file2 = f"{textList[0]}_Draft.yml"
-#
-# print("file1 = ", file1)
-# print("file2 = ", file2)
-#
-# print("path1 = ", f"{pathFull}/{file1}")
-# print("path2 = ", f"{pathFull}/{file2}")
-#
-# with open(f"{pathFull}/{file1}", "w") as f:
-#     f.write("")
-#
-# with open(f"{pathFull}/{file2}", "w") as f:
-#     f.write("")
+nodesList = sys.stdin.read()
+print(nodesList)
 
-# ovpn, hydra, ipsec,
+fileNodes = "/tmp/nodesList.txt"
+
+print("fileNodes = ", fileNodes)
+
+with open(f"{fileNodes}", "w") as f:
+    f.write(nodesList)
--- qqTC/qqm.sh
+++ qqTC/qqm.sh
@@ -4,13 +4,16 @@
 
 #echo $list > /Users/zhuk/myscripts/qqTC/nodes.txt
 
-cat nodes.txt |egrep -v "ipsec|hydra|ovpn"|sort|uniq > nodes_sort.txt
-cat nodes_sort.txt
+python3 /Users/zhuk/myscripts/qqTC/createListNodes.py
+
+cat /tmp/nodesList.txt |egrep -v "ipsec|hydra|ovpn"|sort|uniq > /tmp/nodes_sort.txt
+cat /tmp/nodes_sort.txt
+
 cd /Users/zhuk/SIBEDGE/PROJECT/
 rm -rf ./static-vars-testcenters 2>/dev/null && git clone git@github.com:AnchorFreePartner/static-vars-testcenters.git
 cd static-vars-testcenters
 
-file="/Users/zhuk/myscripts/qqTC/nodes_sort.txt"
+file="/tmp/nodes_sort.txt"
 for server in $(cat $file)
 do
     grep -riwn $server .
